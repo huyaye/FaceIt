888f080d276f8a2a51e02218284b97764581e5dc
huyaye <huyaye@naver.com>
2016-10-02 17:37:42 +0900

    Add property listener (didSet) to redraw when property is changed.

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -11,17 +11,19 @@ import UIKit
 @IBDesignable
 class FaceView: UIView {
     @IBInspectable
-    var scale: CGFloat = 0.90
+    var scale: CGFloat = 0.90 { didSet { setNeedsDisplay() } }
+    // 1 full smile, -1 full frown
     @IBInspectable
-    var mouthCurvature: Double = 1.0    // 1 full smile, -1 full frown
+    var mouthCurvature: Double = 1.0  { didSet { setNeedsDisplay() } }
     @IBInspectable
-    var eyesOpen: Bool = true
+    var eyesOpen: Bool = true  { didSet { setNeedsDisplay() } }
+    // -1 full furrow, 1 fully relaxed
     @IBInspectable
-    var eyeBrowTit: Double = -0.5    // -1 full furrow, 1 fully relaxed
+    var eyeBrowTit: Double = -0.5
     @IBInspectable
-    var color: UIColor = UIColor.blue
+    var color: UIColor = UIColor.blue  { didSet { setNeedsDisplay() } }
     @IBInspectable
-    var lineWidth: CGFloat = 5.0
+    var lineWidth: CGFloat = 5.0  { didSet { setNeedsDisplay() } }
     
     fileprivate var skullRadius: CGFloat {
         return min(bounds.size.width, bounds.size.height) / 2 * scale
