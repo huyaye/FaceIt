19f13670cfa07c458b2b70ac8a0dc73560570599
huyaye <huyaye@naver.com>
2016-10-02 17:56:29 +0900

    Add model for Face view

---
+++ FaceIt/FacialExpression.swift
@@ -0,0 +1,52 @@
+//
+//  FacialExpression.swift
+//  FaceIt
+//
+//  Created by 유정욱 on 2016. 10. 2..
+//  Copyright © 2016년 jungwook.ryu. All rights reserved.
+//
+
+import Foundation
+
+// UI-independent representation of a facial expression
+
+struct FacialExpression
+{
+    enum Eyes: Int {
+        case Open
+        case Closed
+        case Squinting
+    }
+    
+    enum EyeBrows: Int {
+        case Relaxed
+        case Normal
+        case Furrowed
+        
+        func moreRelaxedBrow() -> EyeBrows {
+            return EyeBrows(rawValue: rawValue - 1) ?? .Relaxed
+        }
+        func moreFurrowedBrows() -> EyeBrows {
+            return EyeBrows(rawValue: rawValue + 1) ?? .Furrowed
+        }
+    }
+    
+    enum Mouth: Int {
+        case Frown
+        case Smirk
+        case Neutral
+        case Grin
+        case Smile
+        
+        func sadderMouth() -> Mouth {
+            return Mouth(rawValue: rawValue - 1) ?? .Frown
+        }
+        func happierMouth() -> Mouth {
+            return Mouth(rawValue: rawValue + 1) ?? .Smile
+        }
+    }
+    
+    var eyes: Eyes
+    var eyeBrows: EyeBrows
+    var mouth: Mouth
+}
